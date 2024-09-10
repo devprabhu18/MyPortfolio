40cde3d731237edf6a81749df8866a4bd5450682
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-10 22:48:04 +0530

    Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="mb-3 pt-3 border-t">
+        <div className="flex flex-row space-x-4 ml-8">
+            <div>
+                <a className="flex items-center" href="https://github.com/devprabhu18">
+                    <FaGithub/> <p>Github</p>
+                </a>
+            </div>
+            <div>
+                <a className="flex items-center" href="https://github.com/devprabhu18">
+                    <FaLinkedin/> <p>LinkedIn</p>
+                </a>
+            </div>
+        </div>
+        <div className="flex flex-col content-center justify-center">
+            <p className="flex flex-row content-center justify-center">&copy;Devananda M Prabhu.&nbsp;&nbsp;
+                <span className="font-bold underline underline-offset-4 hover:no-underline">
+                    <Link href="https://github.com/devprabhu18/MyPortfolio" target="_blank">Crafted by yours truly</Link>
+                </span></p>
+        </div>
+      </footer>
+    </>
+  );
+}
