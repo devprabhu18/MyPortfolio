5a28d612b35d02274a10ea78adbf7d844fbe3de6
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-11 22:43:31 +0530

    Updated styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,20 +11,20 @@ const navItems={
 
 export default function Navbar(){
     return(
-        <>
-        <div className=" border-b-4">
-            <div className="ml-10 sticky top-1 mt-2 mb-2">
-                <nav className="flex flex-row space-x-10" id="nav">
-                    <div className="flex flex-row space-x-10">
-                        {Object.entries(navItems).map(([path,{name}])=>(
-                            <Link href={path} key={path}>
-                                {name}
-                            </Link>
-                        )
-                        )}
-                    </div>
-                </nav>
-            </div>
+        <>  
+        <div className="pl-10 sticky top-0 border-b-2 pr-10">
+            <nav className="flex flex-row space-x-10 justify-between pb-3 pt-3" id="nav">
+                <div>
+                    <Link href="/"><h2 className="font-sans text-2xl">&lt;Devananda Prabhu/&gt;</h2></Link>
+                </div>
+                <div className="flex flex-row space-x-10">
+                    {Object.entries(navItems).map(([path,{name}])=>(
+                        <Link href={path} key={path}>
+                            {name}
+                        </Link>
+                    ))}
+                </div>
+            </nav>
         </div>
         </>
     )
