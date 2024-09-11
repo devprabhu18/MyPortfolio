ef8e72b57aa4c017047b59c5ee19d4d709e38d69
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-11 22:42:47 +0530

    Updated styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,27 +1,26 @@
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { ButtonLink } from "@/ui/button";
 
 export default function Footer() {
   return (
     <>
       <footer className="mb-3 pt-3 border-t pl-8 pr-8">
-        <div className="flex flex-row justify-between">
-            <div className="flex flex-row space-x-4 ">
-                <div>
-                    <a className="flex items-center" href="https://github.com/devprabhu18">
+        <div className="flex flex-row justify-between pb-3">
+            <div className="flex flex-row space-x-14 ">
+                <div className="border border-grey rounded-md border-solid p-2 hover:bg-black hover:text-white">
+                    <a className="flex items-center space-x-1" href="https://github.com/devprabhu18">
                         <FaGithub/> <p>Github</p>
                     </a>
                 </div>
-                <div className="">
-                    <a className="flex items-center" href="https://github.com/devprabhu18">
+                <div className="border border-grey rounded-md border-solid p-2">
+                    <a className="flex items-center space-x-1" href="https://github.com/devprabhu18">
                         <FaLinkedin/> <p>LinkedIn</p>
                     </a>
                 </div>
             </div>   
-        <div >
-            <ButtonLink title="Contact me" href="/contact"></ButtonLink>
+        <div className="border border-grey rounded-md border-solid p-2">
+            <Link href="/contact">Contact Me</Link>
         </div>
         </div>
         <div className="flex flex-col content-center justify-center">
