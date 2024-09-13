39d48b3e44a666c70091c006c5338fc03b460e35
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-13 22:15:03 +0530

    Added github image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['github.com'], // Add github.com as an allowed domain
+      },
+};
 
 export default nextConfig;
