830712c463462d91d27ce9ccb8e19579ca05c9fd
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-14 15:37:49 +0530

    Added data for langauges tech stack

---
+++ src/data/languages.tsx
@@ -0,0 +1,35 @@
+import { IoLogoJavascript } from "react-icons/io5";
+import { LiaJava } from "react-icons/lia";
+import {
+  TbBrandCpp,
+  TbBrandTypescript,
+  TbBrandPython,
+} from 'react-icons/tb';
+
+export const languages = [
+  {
+    title: 'C++',
+    Icon: TbBrandCpp,
+    href: 'https://www.cplusplus.com/',
+  },
+  {
+    title: 'JavaScript',
+    Icon: IoLogoJavascript,
+    href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
+  {
+    title: 'TypeScript',
+    Icon: TbBrandTypescript,
+    href: 'https://www.typescriptlang.org/',
+  },
+  {
+    title: 'Python',
+    Icon: TbBrandPython,
+    href: 'https://www.python.org/',
+  },
+  {
+    title: 'Java',
+    Icon: LiaJava,
+    href: 'https://www.java.com/',
+  },
+];
