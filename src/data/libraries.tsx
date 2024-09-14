9540333bded0dd9aa6adc65efe3400a585685361
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-14 20:37:52 +0530

    Added Libraries data

---
+++ src/data/libraries.tsx
@@ -0,0 +1,64 @@
+import { IoLogoNodejs } from 'react-icons/io5';
+import { SiExpress ,SiExpo ,SiFlask, SiFirebase   } from "react-icons/si";
+import {
+  TbBrandNextjs,
+  TbBrandReact,
+  TbBrandTailwind,
+  TbBrandMongodb,
+  TbBrandMysql,
+} from 'react-icons/tb';
+
+export const libraries = [
+  {
+    title: 'Next.js',
+    Icon: TbBrandNextjs,
+    href: 'https://nextjs.org/',
+  },
+  {
+    title: 'React.js',
+    Icon: TbBrandReact,
+    href: 'https://reactjs.org/',
+  },
+  {
+    title: 'Node.js',
+    Icon: IoLogoNodejs,
+    href: 'https://nodejs.org/',
+  },
+  {
+    title: 'Express.js',
+    Icon: SiExpress,
+    href: 'https://expressjs.com/',
+  },
+  {
+    title: 'MongoDB',
+    Icon: TbBrandMongodb,
+    href: 'https://www.mongodb.com/',
+  },
+  {
+    title: 'MySQL',
+    Icon: TbBrandMysql,
+    href: 'https://www.mysql.com/',
+  },
+  {
+    title: 'TailwindCSS',
+    Icon: TbBrandTailwind,
+    href: 'https://tailwindcss.com/',
+  },
+  
+  {
+    title: 'Flask',
+    Icon: SiFlask,
+    href: 'https://flask.palletsprojects.com/en/3.0.x/',
+  },
+  
+  {
+    title: 'Expo',
+    Icon: SiExpo ,
+    href: 'https://expo.dev/',
+  },
+  {
+    title: 'Firebase',
+    Icon: SiFirebase ,
+    href: 'https://firebase.google.com/',
+  },
+];
