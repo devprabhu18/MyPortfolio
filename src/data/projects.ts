83cddfba8dc687616f62bfbc63b7115a480f5a39
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-16 00:11:22 +0530

    Added Project Details

---
+++ src/data/projects.ts
@@ -0,0 +1,49 @@
+export const projects = [
+    {
+      title: 'PartDime',
+      description:
+        'PartDime is a cross-platform mobile app built with React Native that connects people with part-time job opportunities. Users can easily find and post job listings, making it simple to discover and apply for part-time positions. Designed to be intuitive and efficient, PartDime helps bridge the gap in the part-time job market by offering a streamlined and accessible platform for both job seekers and employers.',
+      gitLink: 'https://github.com/devprabhu18/PartDImeApp',
+      prodLink: 'https://github.com/devprabhu18/PartDImeApp',
+      techStack: [
+        'React Native',
+        'Expo',
+        'JavaScript',
+        'Firebase',
+        'Nativewind',
+        'Git',
+      ],
+      thumb: '/images/PartDime.jpg',
+    },
+    {
+      title: 'Portfolio',
+      description:
+        'Crafted using Next.js, this responsive portfolio website showcases my skills, projects, and achievements. The site delivers a seamless user experience, featuring fast page loads, optimized content, and intuitive navigation. Built with modern web technologies, it reflects my expertise in full-stack development and serves as a central hub for exploring my work, connecting with me, and staying updated on my latest endeavors.',
+      gitLink: 'https://github.com/devprabhu18/MyPortfolio',
+      prodLink: 'https://github.com/devprabhu18/MyPortfolio',
+      techStack: [
+        'TypeScript',
+        'Next.js',
+        'TailwindCSS',
+        'Git',
+      ],
+      thumb: '/images/Portfolio.jpg',
+    },
+    {
+      title: 'HotelFlow',
+      description:
+        'hotelFlow is a hotel management web app built with Flask and MySQL. It streamlines receptionist tasks by simplifying check-ins, check-outs, and customer record management. Features include real-time availability tracking, booking management, and reporting tools, all designed to boost efficiency and reduce errors. Perfect for both small and large hotels, hotelFlow enhances productivity and guest experience.',
+      gitLink: 'https://github.com/devprabhu18/HotelFlow',
+      prodLink: 'https://github.com/devprabhu18/HotelFlow',
+      techStack: [
+        'Flask',
+        'MySQL',
+        'Python',
+        'HTML',
+        'CSS',
+        'Git',
+      ],
+      thumb: '/images/HotelFlow.jpg',
+    }
+
+    ];
