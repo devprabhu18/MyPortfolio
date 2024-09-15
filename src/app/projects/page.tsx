5d2e59c3e29e2c3ae071494540059a7965197702
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-15 18:06:05 +0530

    Added a projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,21 @@
+import { ProjectCard } from "@/components/projectcard"
+import { projects } from "@/data/projects"
+
 export default function Page(){
     return(
-        <h1>Dev</h1>
+        <>
+        <div className=" pt-4 sm:pt-2 grid grid-cols-1 pb-4 sm:pb-8  sm:mt-6 mt-0 ml-2 mr-2">
+          <div className="flex flex-col  items-center">
+            
+            <h1 className="font-medium text-2xl sm:text-3xl mt-1 sm:mt-2">Projects</h1>
+            
+            <div className="mt-5 sm:mt-3 grid grid-cols-1 gap-3">
+                {projects.map((project) => (
+                <ProjectCard key={project.title} {...project} />
+                ))}
+            </div>
+        </div>
+        </div>
+        </>
     )
 }
