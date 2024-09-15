cf2c264ca005d9acc50a5a0644dfdc9a2363746e
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-16 00:13:29 +0530

    Enhanced responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,12 +9,12 @@ export default function Footer() {
         <footer className="mb-0 sm:mb-3 pt-4 bg-white/5 border-opacity-5 border-t border-bordercol px-5 sm:px-10 flex flex-col flex-shrink-0">
           <div className="flex flex-col sm:flex-row justify-between pb-3">
             <div className="flex flex-col sm:flex-row sm:space-x-14 space-y-3 sm:space-y-0">
-              <div className="border-[1px] rounded-md border-none bg-white/10 shadow-md hover:shadow-blue-800 hover:-translate-y-0.5 transition-all ease-out">
+              <div className="border-[1px] rounded-md border-none bg-white/10 shadow-md hover:shadow-blue-800 hover:-translate-y-0.5 transition-all ease-out flex flex-row content-center justify-center">
                 <a className="flex items-center p-2 space-x-1" href="https://github.com/devprabhu18">
                   <FaGithub /> <p>Github</p>
                 </a>
               </div>
-              <div className="border-[1px] rounded-md border-none bg-white/10 shadow-md hover:shadow-blue-800 hover:-translate-y-0.5 transition-all ease-out">
+              <div className="border-[1px] rounded-md border-none bg-white/10 shadow-md hover:shadow-blue-800 hover:-translate-y-0.5 transition-all ease-out flex flex-row content-center justify-center">
                 <a className="flex p-2 items-center space-x-1" href="https://www.linkedin.com/in/devanandamprabhu/">
                   <FaLinkedin /> <p>LinkedIn</p>
                 </a>
@@ -22,7 +22,7 @@ export default function Footer() {
             </div>
             <div className="mt-3 sm:mt-0">
               <Link href="/contact">
-                <div className="border-[1px] rounded-md border-none bg-white/10 p-2 shadow-md hover:shadow-blue-800 hover:-translate-y-0.5 transition-all ease-out">
+                <div className="border-[1px] rounded-md border-none bg-white/10 p-2 shadow-md hover:shadow-blue-800 hover:-translate-y-0.5 transition-all ease-out flex flex-row content-center justify-center">
                   Contact Me
                 </div>
               </Link>
