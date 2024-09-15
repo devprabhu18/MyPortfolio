512ff7fcdd37ed79a85dbdd8614d362e9cfe6da6
Devananda M Prabhu <devanandaprabhu18@gmail.com>
2024-09-15 22:52:51 +0530

    Added Social Media Details"

---
+++ src/data/social.tsx
@@ -0,0 +1,35 @@
+import {
+    TbBrandGithub ,
+    TbMail ,
+    TbBrandLinkedin ,
+    TbBrandTwitter ,
+    TbBrandInstagram 
+} from 'react-icons/tb';
+
+export const social = [
+  {
+    title: 'GitHub',
+    Icon: TbBrandGithub ,
+    href: 'https://www.github.com/devprabhu18',
+  },
+  {
+    title: 'Mail',
+    Icon: TbMail ,
+    href: 'mailto:devanandaprabhu18@gmail.com',
+  },
+  {
+    title: 'Linkedin',
+    Icon: TbBrandLinkedin ,
+    href: 'https://www.linkedin.com/in/devanandamprabhu/',
+  },
+  {
+    title: 'Twitter',
+    Icon: TbBrandTwitter ,
+    href: 'https://x.com/devprabhu018',
+  },
+  {
+    title: 'Instagram',
+    Icon: TbBrandInstagram ,
+    href: 'https://www.instagram.com/devanand_.prabhu/',
+  },
+];
